d958c6e7e851f2776fec425021912e6195cfc656
Iswaryadevi98 <51690701+Iswaryadevi98@users.noreply.github.com>
2019-06-14 16:04:58 +0530

    Create countalphabetno.py

---
+++ codeketa/countalphabetno.py
@@ -0,0 +1,18 @@
+h=input()
+d=0
+for i in range(0,len(h)):
+  if h[i].isalpha():
+    d=d+1
+    break
+for i in range(0,len(h)):
+  if h[].isdigit():
+    d=d+1
+    break
+if(d==2):
+  print("Yes")
+else:
+  print("No")
+
+
+
+  
