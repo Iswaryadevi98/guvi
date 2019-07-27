467cbb1d505c488d6210707c4cd329851d22f4cb
Iswaryadevi98 <51690701+Iswaryadevi98@users.noreply.github.com>
2019-07-27 23:51:39 +0530

    Update nrepeatno.py

--- codeketa/nrepeatno.py
+++ codeketa/nrepeatno.py
@@ -1,9 +1,9 @@
+
 def R(x): 
     l= len(x) 
     r= [] 
     for i in range(l): 
-        k = i + 1
-        for j in range(k, l): 
+        for j in range(i+1, l): 
             if x[i] == x[j] and x[i] not in r: 
                 r.append(x[i]) 
     return r 
@@ -11,4 +11,5 @@ n=int(input())
 l1 = list(map(int,input().split()))[:n]
 l2=R(l1)
 l2.sort()
-print(l2) 
+for i in l2:
+  print(i,end='')
